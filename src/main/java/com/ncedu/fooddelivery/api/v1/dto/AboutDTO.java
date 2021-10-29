e5ed76cfb3fad733f20ecba0e9d452ef94eecfbf
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-10-29 16:51:14 +0300

    Для endpoint 'about' добавлено поле 'dbName'. Добавлен сервис TestDbConnect для получения имени БД.

--- src/main/java/com/ncedu/fooddelivery/api/v1/dto/AboutDTO.java
+++ src/main/java/com/ncedu/fooddelivery/api/v1/dto/AboutDTO.java
@@ -5,6 +5,15 @@ public final class AboutDTO {
     private final String PROJECT = "food-delivery";
     private final String[] AUTHORS = {"Michael Dmitrichenko", "Michael Shchenev", "Alex Podshivalov"};
     private final String COACH = "Leonid Fedotov";
+    private String dbName;
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
 
     public String getPROJECT() {
         return PROJECT;
