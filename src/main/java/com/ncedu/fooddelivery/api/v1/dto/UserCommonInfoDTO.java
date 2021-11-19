1f4d6cf88d20848f3ab0a35334f219a8f48b414d
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-11-19 20:33:10 +0300

    UserCommonInfoDTO: Удалены ненужные методы конвертации пароля

--- src/main/java/com/ncedu/fooddelivery/api/v1/dto/UserCommonInfoDTO.java
+++ src/main/java/com/ncedu/fooddelivery/api/v1/dto/UserCommonInfoDTO.java
@@ -10,21 +10,6 @@ public class UserCommonInfoDTO {
     private Timestamp lastSigninDate;
     private UUID avatarId;
 
-
-    public static String encodeHexString(byte[] byteArray) {
-        StringBuffer hexStringBuffer = new StringBuffer();
-        for (int i = 0; i < byteArray.length; i++) {
-            hexStringBuffer.append(byteToHex(byteArray[i]));
-        }
-        return hexStringBuffer.toString();
-    }
-    private static String byteToHex(byte num) {
-        char[] hexDigits = new char[2];
-        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
-        hexDigits[1] = Character.forDigit((num & 0xF), 16);
-        return new String(hexDigits);
-    }
-
     public UserCommonInfoDTO(String role, String fullName, String email, Timestamp lastSigninDate, UUID avatarId) {
         this.role = role;
         this.fullName = fullName;
