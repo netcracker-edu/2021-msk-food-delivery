31f398442eacb740c0759a0093aae65be9f2f5f9
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-11-27 14:33:53 +0300

    + кастомная валидация типа Enum

--- src/main/java/com/ncedu/fooddelivery/api/v1/dto/RegistrationDTO.java
+++ src/main/java/com/ncedu/fooddelivery/api/v1/dto/RegistrationDTO.java
@@ -1,5 +1,6 @@
 package com.ncedu.fooddelivery.api.v1.dto;
 
+import com.ncedu.fooddelivery.api.v1.validators.EnumValid;
 import lombok.Data;
 
 import javax.validation.constraints.*;
@@ -12,6 +13,10 @@ public class RegistrationDTO {
 
     //common user info
     @NotBlank
+    @EnumValid(
+          enumClazz = com.ncedu.fooddelivery.api.v1.entities.Role.class,
+          message = "Role is not valid"
+    )
     private String role;
     @NotBlank
     @Size(min=8, max=64)
@@ -33,6 +38,7 @@ public class RegistrationDTO {
     private Double rating;
 
     //moderator additional fields
+    @Min(value = 1)
     private Long warehouseId;
 
     private static final String EMAIL_REGEXP = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
