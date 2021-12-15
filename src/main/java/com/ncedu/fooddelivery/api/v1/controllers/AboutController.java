629f29b891d737a8d02dc4c4bf4c00242b1dd3ba
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-12-15 08:04:36 +0300

    + логирование log4j через lombok @Slf4j для AboutController

--- src/main/java/com/ncedu/fooddelivery/api/v1/controllers/AboutController.java
+++ src/main/java/com/ncedu/fooddelivery/api/v1/controllers/AboutController.java
@@ -2,11 +2,13 @@ package com.ncedu.fooddelivery.api.v1.controllers;
 
 import com.ncedu.fooddelivery.api.v1.dto.AboutDTO;
 import com.ncedu.fooddelivery.api.v1.services.TestDbConnect;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class AboutController {
 
@@ -15,6 +17,7 @@ public class AboutController {
 
     @GetMapping("/api/v1/about")
     public AboutDTO about() {
+        log.info("GET /api/v1/about calling.");
         AboutDTO aboutDTO = new AboutDTO();
         aboutDTO.setDbName(testDbConnect.getDbName());
         return aboutDTO;
