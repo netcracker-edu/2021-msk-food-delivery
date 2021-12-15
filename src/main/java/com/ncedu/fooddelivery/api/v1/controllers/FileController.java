224a87873c25abd0453680ea5a97bc6fd10b0667
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-12-15 08:47:07 +0300

    + логирование для FileController

--- src/main/java/com/ncedu/fooddelivery/api/v1/controllers/FileController.java
+++ src/main/java/com/ncedu/fooddelivery/api/v1/controllers/FileController.java
@@ -5,6 +5,7 @@ import com.ncedu.fooddelivery.api.v1.entities.Role;
 import com.ncedu.fooddelivery.api.v1.entities.User;
 import com.ncedu.fooddelivery.api.v1.errors.security.CustomAccessDeniedException;
 import com.ncedu.fooddelivery.api.v1.services.FileService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -21,6 +22,7 @@ import java.util.Map;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@Slf4j
 @RestController
 public class FileController {
 
@@ -31,11 +33,13 @@ public class FileController {
     public Map<String,String> uploadFile (
             @RequestParam("file") MultipartFile file,
             @AuthenticationPrincipal User authedUser) {
+        log.info("POST /api/v1/file");
         String fileUUID = fileService.save(file, authedUser);
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/api/v1/download/")
+                .path("/api/v1/file/")
                 .path(fileUUID)
                 .toUriString();
+        log.info("Created file link: " + fileDownloadUri);
         Map<String, String> response = new HashMap<>();
         response.put("file", fileDownloadUri);
         return response;
@@ -44,13 +48,15 @@ public class FileController {
     public ResponseEntity<Resource> download(
             @PathVariable File file,
             @AuthenticationPrincipal User authedUser) {
-
+        log.info("GET /api/v1/file/" + file.getId().toString());
         Resource resource = fileService.load(file);
         String mediaType = file.getType().getMediaType();
+        String fileNameWithExt = file.getName() + "." + file.getType().name();
+        log.info("Sending file "+fileNameWithExt+"("+file.getId().toString() +")"+" to client");
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(mediaType))
                 .header(HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment; filename=\"" + file.getName() +"."+ file.getType().name() +"\"")
+                        "attachment; filename=\"" + fileNameWithExt +"\"")
                 .body(resource);
     }
 
@@ -58,14 +64,17 @@ public class FileController {
     public ResponseEntity<?> delete(
             @PathVariable File file,
             @AuthenticationPrincipal User authedUser) {
+        log.info("DELETE /api/v1/file/" + file.getId().toString());
 
         Long fileOwnerId = file.getOwner().getId();
         boolean isNotAdmin = !Role.isADMIN(authedUser.getRole());
         boolean isNotOwner = !fileOwnerId.equals(authedUser.getId());
         if (isNotAdmin && isNotOwner) {
+            log.error(authedUser.getEmail() + " not Admin and not Owner of the file " + file.getId().toString());
             throw new CustomAccessDeniedException();
         }
         fileService.delete(file);
+        log.info("File deleted: " + file.getId().toString());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
