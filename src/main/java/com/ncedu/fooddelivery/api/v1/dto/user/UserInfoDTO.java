64ee5f2c58a8009ea3bca3fc8ee905fab92fa48b
Alex Podshivalov <ksndr.pdsh@gmail.com>
2022-02-03 07:49:11 +0300

    + поле lockDate в UserInfoDTO

--- src/main/java/com/ncedu/fooddelivery/api/v1/dto/user/UserInfoDTO.java
+++ src/main/java/com/ncedu/fooddelivery/api/v1/dto/user/UserInfoDTO.java
@@ -1,11 +1,13 @@
 package com.ncedu.fooddelivery.api.v1.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class UserInfoDTO {
     private Long id;
     private String role;
@@ -13,6 +15,7 @@ public class UserInfoDTO {
     private String email;
     private Timestamp lastSigninDate;
     private UUID avatarId;
+    private Timestamp lockDate;
 
     public UserInfoDTO(Long id, String role, String fullName, String email, Timestamp lastSigninDate, UUID avatarId) {
         this.id = id;
