790c7ab3c8c29d2680a1be03e9f54bd0fc03060a
grey3228 <m.dmitrichenko00@bk.ru>
2022-03-28 23:03:59 +0300

    Fix to OrderClient

--- front/src/api/OrderClient.js
+++ front/src/api/OrderClient.js
@@ -15,31 +15,31 @@ export default class OrderClient{
     }
 
     async getOrderById(id){
-        this.checkToken();
-        return await commonFetch(this.config.ORDER_URL + `/${id}`, 'GET', 
+        await this.checkToken();
+        return commonFetch(this.config.ORDER_URL + `/${id}`, 'GET', 
         this.config.headersWithAuthorization(), null);
     }
 
     async changeOrderStatus(orderId, newStatus){
-        this.checkToken();
-        return await patchFetch(this.config.ORDER_URL + `/${orderId}/status`, 
+        await this.checkToken();
+        return patchFetch(this.config.ORDER_URL + `/${orderId}/status`, 
         this.config.headersWithAuthorization(), JSON.stringify({newStatus: newStatus}));
     }
 
     async changeRating(orderId, role, newRating){
-        this.checkToken();
+        await this.checkToken();
         return patchFetch(this.config.ORDER_URL + `/${orderId}/` + (role === 'COURIER' ? 'clientRating' : 'courierRating'),
         this.config.headersWithAuthorization(), JSON.stringify({rating: newRating}));
     }
 
     async getOverallOrdersAmount(){
-        this.checkToken();
-        return await commonFetch(this.config.ORDERS_AMOUNT_URL, 'GET', this.config.headersWithAuthorization(), null);
+        await this.checkToken();
+        return commonFetch(this.config.ORDERS_AMOUNT_URL, 'GET', this.config.headersWithAuthorization(), null);
     }
 
     async fetchOrderPage(page, size){
-        this.checkToken();
-        return await commonFetch(this.buildPaginationQuery(page, size), 'GET', 
+        await this.checkToken();
+        return commonFetch(this.buildPaginationQuery(page, size), 'GET', 
         this.config.headersWithAuthorization(), null);
     }   
 
