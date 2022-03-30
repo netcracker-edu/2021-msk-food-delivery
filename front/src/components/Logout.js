dc22d3fc0b5ced31c9e6001c9609de849f156fcb
Alex Podshivalov <ksndr.pdsh@gmail.com>
2022-03-30 20:16:35 +0300

    + удаление всех данных из localStorage при logout

--- front/src/components/Logout.js
+++ front/src/components/Logout.js
@@ -1,8 +1,11 @@
 import {useNavigate} from 'react-router-dom';
 import {useEffect} from 'react';
+import {updateCart, useCartContext } from "../hooks/CartContext";
 
-const Logout = ({auth}) => {
+const Logout = ({auth, setAddress}) => {
   const navigate = useNavigate();
+  const { cartItems, dispatch } = useCartContext();
+
   const logout = async () => {
     if (auth.token) {
       let refreshToken = { "refreshToken" : auth.token.refreshToken };
@@ -13,6 +16,8 @@ const Logout = ({auth}) => {
 
   useEffect(() => {
     logout();
+    setAddress({});
+    dispatch(updateCart({}));
     navigate("/");
   });
 
